a63a1f1cd83461bd795282dde1b1d3c0ba9f6b4d
Jeff-wu-hub <15809812381@163.com>
2020-12-24 18:20:59 +0800

    数据库测试

--- controller/userControl.js
+++ controller/userControl.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql')
 module.exports = {
     /*
     * 用户登录接口控制*/
@@ -6,6 +7,21 @@ module.exports = {
             user: req.body.user,
             password: req.body.password
         }
+        query(obj)
         res.send(obj) // 返回数据
     }
+}
+query = function (obj){
+    let db = mysql.createConnection({
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
+        password: 'wgs19971211',
+        database: 'nodedatabase'
+    })
+    db.connect()
+    db.query('SELECT * FROM user WHERE user = ? AND password = ?', [obj.user, obj.password], (err, data) => {
+        console.log(data)
+
+    })
 }
